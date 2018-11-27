71a26bd24aad8a7477aa98aa945d37fa1ccc9585
Eric Chong <echong@microsoft.com>
2018-11-27 14:41:29 -0800

    Refactored validation logic

--- test/jsonValidationTest.js
+++ test/jsonValidationTest.js
@@ -5,40 +5,41 @@ const fs = require('fs');
 describe('Validating Cohorts...', () => {
     const cohortPath = './Cohorts';
 
-    it('Verifying .cohort files', function(done){
+    it('Verifying .cohort files', function (done) {
         let failedList = [];
         browseDirectory(cohortPath, (error, results) => {
             results.filter(file => file.substr(-7) === '.cohort')
-                .forEach(file => {                    
-                    validateJsonString(file)
+                .forEach(file => {
+                    validateJsonStringAndGetObject(file)
                 });
-            
-                done();
+
+            done();
         });
     });
 
-    it('Verifying cohort settings files', function(done){
+    it('Verifying cohort settings files', function (done) {
         browseDirectory(cohortPath, (error, results) => {
             if (error) throw error;
             results.filter(file => file.substr(-13) === 'settings.json')
                 .forEach(file => {
-                    let json = validateJsonString(file);
-                    validateSettingsForCohort(json);
+                    let json = validateJsonStringAndGetObject(file);
+                    validateSettingsForCohort(json, file);
                 });
-            
-                done();
+
+            done();
         });
     });
 
-    it('Verifying cohort category files', function(done){
+    it('Verifying cohort category files', function (done) {
         browseDirectory(cohortPath, (error, results) => {
             if (error) throw error;
             results.filter(file => file.substr(-22) === 'categoryResources.json')
                 .forEach(file => {
-                    validateJsonString(file);
+                    let json = validateJsonStringAndGetObject(file);
+                    validateCategory(json, file);
                 });
-            
-                done();
+
+            done();
         });
     });
 });
@@ -46,127 +47,125 @@ describe('Validating Cohorts...', () => {
 describe('Validating Workbooks...', () => {
     const cohortPath = './Workbooks';
 
-    it('Verifying .workbook files', function(done){
+    it('Verifying .workbook files', function (done) {
         let failedList = [];
         browseDirectory(cohortPath, (error, results) => {
             results.filter(file => file.substr(-9) === '.workbook')
-                .forEach(file => {                    
-                    validateJsonString(file)
+                .forEach(file => {
+                    validateJsonStringAndGetObject(file)
                 });
-            
-                done();
+
+            done();
         });
     });
 
-    it('Verifying workbook settings.json files', function(done){
+    it('Verifying workbook settings.json files', function (done) {
         browseDirectory(cohortPath, (error, results) => {
             if (error) throw error;
             results.filter(file => file.substr(-13) === 'settings.json')
                 .forEach(file => {
-                    let json = validateJsonString(file)
-                    validateSettingsForWorkbook(json);
+                    let json = validateJsonStringAndGetObject(file)
+                    validateSettingsForWorkbook(json, file);
                 });
-            
-                done();
+
+            done();
         });
     });
 
-    it('Verifying workbook category json files', function(done){
+    it('Verifying workbook category json files', function (done) {
         browseDirectory(cohortPath, (error, results) => {
             if (error) throw error;
             results.filter(file => file.substr(-22) === 'categoryResources.json')
                 .forEach(file => {
-                    validateJsonString(file);
+                    let json = validateJsonStringAndGetObject(file);
+                    validateCategory(json, file);
                 });
-            
-                done();
+
+            done();
         });
     });
 });
 
-function validateJsonString(file) {
-    let json = fs.readFileSync(file, 'utf8');
-    assert.isTrue(isJsonString(json), 'Invalid json format with \'' + file + '\'');
-    return json;
-}
-
-var browseDirectory = function(dir, done) {
+var browseDirectory = function (dir, done) {
     var results = [];
-    fs.readdir(dir, function(err, list) {
-      if (err) return done(err);
-      var i = 0;
-      (function next() {
-        var file = list[i++];
-        if (!file) return done(null, results);
-        file = dir + '/' + file;
-        fs.stat(file, function(err, stat) {
-          if (stat && stat.isDirectory()) {
-            browseDirectory(file, function(err, res) {
-              results = results.concat(res);
-              next();
+    fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var i = 0;
+        (function next() {
+            var file = list[i++];
+            if (!file) return done(null, results);
+            file = dir + '/' + file;
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    browseDirectory(file, function (err, res) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else {
+                    results.push(file);
+                    next();
+                }
             });
-          } else {
-            results.push(file);
-            next();
-          }
-        });
-      })();
+        })();
     });
-  };
+};
 
-function validateSettingsForCohort(json) {
-    let settings = JSON.parse(json);
-    if (!settings.$schema) {
-        assert.fail("The $schema field is missing");
+function validateJsonStringAndGetObject(file) {
+    let json = fs.readFileSync(file, 'utf8');
+    let obj = TryParseJson(json)
+    assert.isTrue(obj != null, 'Invalid json format with \'' + file + '\'');
+    return obj;
+}
+
+function validateSettingsForCohort(settings) {
+    if (!settings.hasOwnProperty('$schema')) {
+        assert.fail("The $schema field is missing with '" + file + "'");
     }
-    if (!settings.name) {
-        assert.fail("The name field is missing");
+    if (!settings.hasOwnProperty('name')) {
+        assert.fail("The name field is missing with '" + file + "'");
     }
-    if (!settings.author) {
-        assert.fail("The author field is missing");
+    if (!settings.hasOwnProperty('author')) {
+        assert.fail("The author field is missing with '" + file + "'");
     }
 }
 
-function validateSettingsForWorkbook(json) {
-    let settings = JSON.parse(json);
-    if (!settings.$schema) {
-        assert.fail("The $schema field is missing");
+function validateSettingsForWorkbook(settings, file) {
+    if (!settings.hasOwnProperty('$schema')) {
+        assert.fail("The $schema field is missing with '" + file + "'");
     }
-    if (!settings.name) {
-        assert.fail("The name field is missing");
+    if (!settings.hasOwnProperty('name')) {
+        assert.fail("The name field is missing with '" + file + "'");
     }
-    if (!settings.author) {
-        assert.fail("The author field is missing");
+    if (!settings.hasOwnProperty('author')) {
+        assert.fail("The author field is missing with '" + file + "'");
     }
-    if (!settings.galleries) {
-        assert.fail("The galleries field is missing");
+    if (!settings.hasOwnProperty('galleries')) {
+        assert.fail("The galleries field is missing with '" + file + "'");
     }
     if (!Array.isArray(settings.galleries)) {
-        assert.fail("The galleries should be an array");
-    }    
-}
-
-function validateCategory(json) {
-    let category = JSON.parse(json);
-    if (!category.en-us) {
-        assert.fail("The en-us field is missing");
-    }
-    if (!category.en-us.name) {
-        assert.fail("The name field is missing");
-    }
-    if (!category.en-us.description) {
-        assert.fail("The description field is missing");
-    }
-    if (!category.en-us.order) {
-        assert.fail("The order field is missing");
+        assert.fail("The galleries should be an array with '" + file + "'");
     }
 }
 
-function isJsonString(str) {
+function validateCategory(category, file) {
+    if (!category.hasOwnProperty('en-us')) {
+        assert.fail("The en-us field is missing with '" + file + "'");
+    }
+    if (!category['en-us'].hasOwnProperty('name')) {
+        assert.fail("The name field is missing with '" + file + "'");
+    }
+    if (!category['en-us'].hasOwnProperty('description')) {
+        assert.fail("The description field is missing with '" + file + "'");
+    }
+    if (!category['en-us'].hasOwnProperty('order')) {
+        assert.fail("The order field is missing with '" + file + "'");
+    }
+}
+
+function TryParseJson(str) {
     try {
-        JSON.parse(str);
+        return JSON.parse(str);
     } catch (e) {
-        return false;
+        return null;
     }
-    return true;
 }
