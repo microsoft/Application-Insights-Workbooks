2be1eebd8da72a66f2836a07db7d85a0f83872ff
Vu Tran <vutran@microsoft.com>
2019-11-12 15:31:40 -0800

    get each repo

--- scripts/restoreDocsRepos.cmd
+++ scripts/restoreDocsRepos.cmd
@@ -1,6 +1,5 @@
 cd /D "%~dp0"
 dir
-git clone https://github.com/MicrosoftDocs/Application-Insights-Workbooks.cs-cz.git Application-Insights-Workbooks.cs-cz
-dir /s Application-Insights-Workbooks.cs-cz
-git clone https://github.com/MicrosoftDocs/Application-Insights-Workbooks.de-de.git Application-Insights-Workbooks.de-de
-dir /s Application-Insights-Workbooks.de-de
+git clone https://github.com/MicrosoftDocs/%1.git %1
+dir /s %1
+exit 0
