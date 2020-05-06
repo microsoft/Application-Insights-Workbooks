c10562ee374541b3d086af7d986a9f47231c57f8
Erica Lin <erlin@microsoft.com>
2020-05-05 17:25:57 -0700

    fix key naming

--- scripts/localizeWorkbook.js
+++ scripts/localizeWorkbook.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 
 // Keys to localize. Add new keys here.
-const Keys = [
-    "json",
-    "description",
-    "label",
-    "linkLabel",
-    "preText",
-    "postText",
-    "title",
-    "chartTitle",
-    "defaultItemsText",
-    "loadButtonText",
-    "typeSettings",
-    "noDataMessage"
+const LocKeys = [
+  "json",
+  "description",
+  "label",
+  "linkLabel",
+  "preText",
+  "postText",
+  "title",
+  "chartTitle",
+  "defaultItemsText",
+  "loadButtonText",
+  "typeSettings",
+  "noDataMessage"
 ];
 
 const Encoding = 'utf8';
@@ -21,7 +21,7 @@ const StringFileName = 'strings.json';
 const ResxFileName = 'strings.resx';
 
 const ResxBegin =
-    `<?xml version="1.0" encoding="utf-8"?>
+  `<?xml version="1.0" encoding="utf-8"?>
 <root>
   <xsd:schema id="root" xmlns="" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
     <xsd:import namespace="http://www.w3.org/XML/1998/namespace" />
@@ -88,131 +88,130 @@ const ResXEntryEnd = '</data>';
 
 // FUNCTIONS
 function testPath(path) {
-    if (fs.existsSync(path)) {
-        // file exists
-        console.log("Found file...", path);
-        return true;
-    } else {
-        console.error("File doesn't exist");
-        return false;
-    }
+  if (fs.existsSync(path)) {
+    // file exists
+    console.log("Found file...", path);
+    return true;
+  } else {
+    console.error("File doesn't exist");
+    return false;
+  }
 }
 
 
 function isValidFileType(filename) {
-    const parts = filename.split('.');
-    const extension = parts[parts.length - 1];
-    return extension === "workbook" || extension === "cohort";
+  const parts = filename.split('.');
+  const extension = parts[parts.length - 1];
+  return extension === "workbook" || extension === "cohort";
 }
 
 function openWorkbook(file) {
-    try {
-        const data = fs.readFileSync(file, Encoding);
-        return data;
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const data = fs.readFileSync(file, Encoding);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-function getObjects(obj, objKey, outputMap) {
-    const key = (objKey || '').concat('.');
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) {
-            continue;
-        }
-        if (typeof obj[i] == 'object') {
-            getObjects(obj[i], key.concat(i), outputMap);
-        } else if (Keys.includes(i)) {
-            outputMap[key] = obj[i];
-        }
+function getLocalizeableStrings(obj, key, outputMap) {
+  for (var i in obj) {
+    if (!obj.hasOwnProperty(i)) {
+      continue;
     }
+    if (typeof obj[i] == 'object') {
+      getLocalizeableStrings(obj[i], key.concat(i, '.'), outputMap);
+    } else if (LocKeys.includes(i)) {
+      outputMap[key.concat(i)] = obj[i];
+    }
+  }
 }
 
 /** Write string file as JSON */
 function writeToFileJSON(data, path) {
-    const parts = path.split('\\');
-    var directoryPath = '';
-    for (var i = 0; i < parts.length - 1; i++) {
-        directoryPath = directoryPath.concat(parts[i], '\\');
-    }
-    const fullpath = directoryPath.concat(StringFileName);
-    console.log(directoryPath);
-    const content = JSON.stringify(data, null, "\t");
-    try {
-        fs.writeFileSync(fullpath, content);
-        console.log("Wrote to file... ", fullpath);
-        console.log("String file generated. Please check the file in.")
-    } catch (e) {
-        console.log("Cannot write file ", e);
-    }
+  const parts = path.split('\\');
+  var directoryPath = '';
+  for (var i = 0; i < parts.length - 1; i++) {
+    directoryPath = directoryPath.concat(parts[i], '\\');
+  }
+  const fullpath = directoryPath.concat(StringFileName);
+  console.log(directoryPath);
+  const content = JSON.stringify(data, null, "\t");
+  try {
+    fs.writeFileSync(fullpath, content);
+    console.log("Wrote to file... ", fullpath);
+    console.log("String file generated. Please check the file in.")
+  } catch (e) {
+    console.log("Cannot write file ", e);
+  }
 }
 
 /** Write string file as ResX format */
 function writeToFileResX(data, path) {
-    const parts = path.split('\\');
-    var directoryPath = '';
-    for (var i = 0; i < parts.length - 1; i++) {
-        directoryPath = directoryPath.concat(parts[i], '\\');
-    }
-    const fullpath = directoryPath.concat(ResxFileName);
-    console.log(directoryPath);
-    try {
-        const writeStream = fs.createWriteStream(fullpath, {
-            flags: 'a'
-        });
+  const parts = path.split('\\');
+  var directoryPath = '';
+  for (var i = 0; i < parts.length - 1; i++) {
+    directoryPath = directoryPath.concat(parts[i], '\\');
+  }
+  const fullpath = directoryPath.concat(ResxFileName);
+  console.log(directoryPath);
+  try {
+    const writeStream = fs.createWriteStream(fullpath, {
+      flags: 'a'
+    });
 
-        return new Promise((resolve, reject) => {
-            writeStream.on("error", reject);
-            writeStream.on("finish", resolve);
+    return new Promise((resolve, reject) => {
+      writeStream.on("error", reject);
+      writeStream.on("finish", resolve);
 
-            // Start of resx file
-            writeStream.write(ResxBegin);
+      // Start of resx file
+      writeStream.write(ResxBegin);
 
-            // Begin writing definitions
-            for (var key in data) {
-                const val = data[key];
-                const definition = `<data name="${key}" xml:space="preserve">`;
-                const value = `<value>"${val}"</value>`;
+      // Begin writing definitions
+      for (var key in data) {
+        const val = data[key];
+        const definition = `<data name="${key}" xml:space="preserve">`;
+        const value = `<value>"${val}"</value>`;
 
-                writeStream.write('\t' + definition + '\r\n');
-                writeStream.write('\t\t' + value + '\r\n');
-                writeStream.write('\t' + ResXEntryEnd + '\r\n');
-            }
+        writeStream.write('\t' + definition + '\r\n');
+        writeStream.write('\t\t' + value + '\r\n');
+        writeStream.write('\t' + ResXEntryEnd + '\r\n');
+      }
 
-            writeStream.write(ResXEnd);
+      writeStream.write(ResXEnd);
 
-            console.log("Wrote to file... ", fullpath);
-            console.log("String file generated. Please check the file in.");
-            writeStream.close();
-        });
+      console.log("Wrote to file... ", fullpath);
+      console.log("String file generated. Please check the file in.");
+      writeStream.close();
+    });
 
-    } catch (e) {
-        console.log("Cannot write file: ", e);
-    }
+  } catch (e) {
+    console.log("Cannot write file: ", e);
+  }
 }
 
 // SCRIPT
 if (!process.argv[2]) {
-    console.log('Workbook path not provided. Please provide the path to the workbook to localize.');
+  console.log('Workbook path not provided. Please provide the path to the workbook to localize.');
 } else {
-    const filePath = process.argv[2];
-    const exists = testPath(filePath);
-    if (!exists) {
-        return;
-    }
+  const filePath = process.argv[2];
+  const exists = testPath(filePath);
+  if (!exists) {
+    return;
+  }
 
-    const isValid = isValidFileType(filePath);
-    if (!isValid) {
-        console.error("The provided path does not contain a workbook. The extension must end with .workbook or .cohort");
-        return;
-    }
-    // Valid workbook, start read the content
-    const data = openWorkbook(filePath);
-    var map = {};
-    getObjects(JSON.parse(data), '', map);
-    console.log(map);
+  const isValid = isValidFileType(filePath);
+  if (!isValid) {
+    console.error("The provided path does not contain a workbook. The extension must end with .workbook or .cohort");
+    return;
+  }
+  // Valid workbook, start read the content
+  const data = openWorkbook(filePath);
+  var map = {};
+  getLocalizeableStrings(JSON.parse(data), '', map);
+  console.log(map);
 
-    // Write new strings to file
-    // writeToFile(map, filePath);
-    writeToFileResX(map, filePath);
+  // Write new strings to file
+  // writeToFile(map, filePath);
+  writeToFileResX(map, filePath);
 }
