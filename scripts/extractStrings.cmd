bb9448bfa1013b18aaa0bedc47e077038065c5e9
Erica Lin <erlin@microsoft.com>
2020-05-28 14:26:57 -0700

    yml and script cmd

--- scripts/extractStrings.cmd
+++ scripts/extractStrings.cmd
@@ -1,3 +1,16 @@
 cd /D "%~dp0"
-cd "..\output\package"
-node localizeWorkbook.js "C:\src\Application-Insights-Workbooks\Workbooks\Network Insights\ApplicationGatewayWorkbooks\Network Insights ApplicationGateways Detailed\NetworkInsights-ApplicationGatewayMetrics.workbook"
+@echo off
+
+set REL_PATH=..\output\package\en-us\Workbooks
+set ABS_PATH=
+
+rem // Save current directory and change to target directory
+pushd %REL_PATH%
+
+rem // Save value of CD variable (current directory)
+set ABS_PATH=%CD%
+
+rem // Restore original directory
+popd
+
+node localizeWorkbook.js %ABS_PATH%
