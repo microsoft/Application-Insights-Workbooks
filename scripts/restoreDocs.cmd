94f34cb48b38415e9f7364c4a4854548a4a0c210
Erica Lin <erlin@microsoft.com>
2020-10-22 11:00:01 -0700

    remove test files

--- scripts/restoreDocs.cmd
+++ scripts/restoreDocs.cmd
@@ -21,5 +21,6 @@ git clone --single-branch --branch master --no-tags https://github.com/Microsoft
 
 REM clone the new localization string-only repository
 git clone --single-branch --branch main --no-tags https://github.com/microsoft/Workbooks-Localization localization
+dir
 
 exit /B 0
