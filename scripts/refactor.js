2539dc20e47694e48b5547737860a367be1b6d1a
Erica Lin <erlin@microsoft.com>
2021-02-25 17:37:49 -0800

    update gallery files

--- scripts/refactor.js
+++ scripts/refactor.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 
-const directories =["C:\\src\\Application-Insights-Workbooks\\output\\package\\en-us\\Cohorts", "C:\\src\\Application-Insights-Workbooks\\output\\package\\en-us\\Workbooks"];
+const directories = ["C:\\src\\Application-Insights-Workbooks\\output\\package\\en-us\\Cohorts", "C:\\src\\Application-Insights-Workbooks\\output\\package\\en-us\\Workbooks"];
 for (var d in directories) {
     const dirPath = directories[d];
     const files = fs.readdirSync(dirPath);
@@ -13,16 +13,36 @@ for (var d in directories) {
             const data = fs.readFileSync(fullPath, 'utf8');
             var jsonParsedData = JSON.parse(data);
             var newData = {};
+            newData["$schema"] = "https://raw.githubusercontent.com/microsoft/Application-Insights-Workbooks/master/schema/gallery.json";
+            newData["version"] = "TemplateGallery/1.0";
             const keys = Object.keys(jsonParsedData);
+            var categories = [];
             keys.forEach(key => {
-                newData[key] = jsonParsedData[key];
-                var templates = newData[key]["templates"];
-                templates.sort((a, b) => (a.order > b.order) ? 1 : -1);
-                templates.forEach(template => {
-                     template["fileName"] = undefined;
-                     template["order"] = undefined;
-                });
+                if (key !== "$schema" && key !== "version" && key !== "categories") {
+                    var category = jsonParsedData[key];
+                    var templates = category["templates"];
+                    templates.sort((a, b) => (a.order > b.order) ? 1 : -1);
+                    templates.forEach(template => {
+                        template["fileName"] = undefined;
+                        template["order"] = undefined;
+                    });
+
+                    var newCategory = {};
+                    newCategory = { "id": key };
+                    var categoryKeys = Object.keys(category);
+                    categoryKeys.forEach(key => {
+                        newCategory[key] = category[key];
+                    });
+                    categories.push(newCategory);
+                }
             });
+
+            categories.sort((a, b) => (a.order > b.order) ? 1 : -1);
+            categories.forEach(c => {
+                c["order"] = undefined;
+            });
+            newData["categories"] = categories;
+
 
             const newlocation = "C:\\src\\Application-Insights-Workbooks\\gallery\\";
             const newFileName = fileName.replace("_gallery.", "");
